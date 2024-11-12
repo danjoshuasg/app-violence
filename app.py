c38b24ad9ed89481eb09ba05401bb23ebd674932
Dan Joshua Santivañez Gutarra <dan.joshua.santivanez@gmail.com>
2024-11-11 23:43:46 -0500

    app modified

--- app.py
+++ app.py
@@ -191,4 +191,4 @@ def logout():
 if __name__ == '__main__':
     #app.run(debug=True)
     port = int(os.environ.get('PORT', 8050))
-    app.run_server(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
